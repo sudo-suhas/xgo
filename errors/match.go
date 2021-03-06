d78b281f7a703ffb151231e87c8d1d8a95b8b74a
Suhas Karanth <sudo.suhas@gmail.com>
2021-03-06 11:56:08 +0530

    Update golangci-lint configuration

--- errors/match.go
+++ errors/match.go
@@ -35,13 +35,10 @@ func Match(err1, err2 error) bool {
 // 	errors.Diff(errors.E(errors.WithOp("service.MakeBooking"), errors.PermissionDenied), err)
 // tests whether err is an Error with Kind=PermissionDenied and
 // Op=service.MakeBooking.
-func Diff(err1, err2 error) []string {
-	e1, ok := err1.(*Error)
-	if !ok {
-		return []string{fmt.Sprintf(`template="%v"(type %[1]T); err="%v"(type %[2]T)`, err1, err2)}
-	}
-	e2, ok := err2.(*Error)
-	if !ok {
+func Diff(err1, err2 error) []string { //nolint: gocognit
+	e1, ok1 := err1.(*Error)
+	e2, ok2 := err2.(*Error)
+	if !ok1 || !ok2 {
 		return []string{fmt.Sprintf(`template="%v"(type %[1]T); err="%v"(type %[2]T)`, err1, err2)}
 	}
 
@@ -61,22 +58,27 @@ func Diff(err1, err2 error) []string {
 	if e1.Data != nil && !reflect.DeepEqual(e1.Data, e2.Data) {
 		a = append(a, fmt.Sprintf(`Data: template="%#v"; err="%#v"`, e1.Data, e2.Data))
 	}
-	if e1.Err != nil {
-		if _, ok := e1.Err.(*Error); ok {
-			b := Diff(e1.Err, e2.Err)
-			if len(b) == 0 {
-				return a
-			}
+	if e1.Err == nil {
+		return a
+	}
 
-			a = append(a, "Diff(template.Err, err.Err)=")
-			for _, d := range b {
-				a = append(a, "\t"+d)
-			}
+	switch e1.Err.(type) {
+	case *Error:
+		b := Diff(e1.Err, e2.Err)
+		if len(b) == 0 {
 			return a
 		}
+
+		a = append(a, "Diff(template.Err, err.Err)=")
+		for _, d := range b {
+			a = append(a, "\t"+d)
+		}
+
+	default:
 		if e2.Err == nil || e2.Err.Error() != e1.Err.Error() {
 			a = append(a, fmt.Sprintf(`Err(nested): template="%v"(type %[1]T); err="%v"(type %[2]T)`, e1.Err, e2.Err))
 		}
 	}
+
 	return a
 }
